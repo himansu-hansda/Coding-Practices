fe97d19cc56b1e6e215c0075dbb750c4a52eacdf
Himansu Hansda <himansu.hansda.10@gmail.com>
2021-08-04 01:10:30 +0530

    master

---
+++ Dynamic-Programming/GridTraveller.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string.h>
+#include<map>
+using namespace std;
+long long gridTraveller(int m,int n,map<string,long long>& memo)
+{
+    string tmp1=to_string(m);
+    string tmp2=to_string(n);
+    string key=tmp1+","+tmp2;
+	if(m==1&&n==1) return 1;
+    if(m==0||n==0) return 0;
+    map<string,long long>::iterator itr=memo.find(key);
+    if(itr->first==key)
+    {
+        return itr->second;
+    }
+    else{
+        memo[key] = gridTraveller(m-1,n,memo) + gridTraveller(m,n-1,memo);
+        return memo[key];
+    }
+
+}
+int main()
+{
+	int n,m;
+	cin>>n>>m;
+    map<string,long long> mp;
+    cout<<gridTraveller(n,m,mp);
+}
